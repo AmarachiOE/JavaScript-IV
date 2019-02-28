0760b783f04ba27cd757cca41e3d02e6163563c8
AmarachiOE <ao.ejiawoko@gmail.com>
2019-02-28 13:02:44 -0800

    person, instructor, and pm classes executed successfully with objects checks. need to edit Student class.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,6 +1,5 @@
 // CODE here for your Lambda Classes
 
-
 /* 
 
 #### Person
@@ -15,17 +14,16 @@
 // Base Class -- PERSON
 
 class Person {
-    constructor(attrs) {
-        this.name = attrs.name;
-        this.age = attrs.age;
-        this.location = attrs.location;
-        this.gender = attrs.gender;
-    }
-    speak (){
-        console.log(`Hello my name is ${this.name}, I am from ${this.location}.`);
-    }
+  constructor(attrs) {
+    this.name = attrs.name;
+    this.age = attrs.age;
+    this.location = attrs.location;
+    this.gender = attrs.gender;
+  }
+  speak() {
+    console.log(`Hello my name is ${this.name}, I am from ${this.location}.`);
+  }
 }
-
 
 /* 
 
@@ -45,20 +43,20 @@ class Person {
 
 // Child Class -- INSTRUCTOR
 class Instructor extends Person {
-    constructor(instructorAttrs) {
-        super(instructorAttrs);
-        this.specialty = instructorAttrs.specialty;
-        this.favLanguage = instructorAttrs.favLanguage;
-        this.catchPhrase = instructorAttrs.catchPhrase;
-    }
-    demo(subject) {
-        console.log(`Today we are learning about ${subject}`);
-    }
-    grade(student, subject) { //maybe remove student?
-        console.log(`${student.name} receives a perfect score on ${subject}.`);
-    }
+  constructor(instructorAttrs) {
+    super(instructorAttrs);
+    this.specialty = instructorAttrs.specialty;
+    this.favLanguage = instructorAttrs.favLanguage;
+    this.catchPhrase = instructorAttrs.catchPhrase;
+  }
+  demo(subject) {
+    console.log(`Today we are learning about ${subject}!`);
+  }
+  grade(student, subject) {
+    //maybe remove student?
+    console.log(`${student.name} receives a perfect score on ${subject}!!!`);
+  }
 }
-
 
 /* 
 
@@ -77,30 +75,30 @@ class Instructor extends Person {
 
 */
 
-
 // Child Class -- STUDENT
 class Student extends Person {
-    constructor(studentAttrs) {
-        super(studentAttrs);
-        this.previousBackground = studentAttrs.previousBackground;
-        this.className = studentAttrs.className;
-        this.favSubjects = studentAttrs.favSubjects;
-    }
-    listsSubjects(subject1, subject2, subject3) {
-        console.log(`Their favorite subjects are ${subject1}, ${subject2}, and ${subject3}.`);
-    }
-    PRAssignment(subject) {
-        console.log(`${this.name} has submitted a PR for ${subject}.`);
-    }
-    sprintChallenge(subject) {
-        console.log(`${this.name} has begun sprint challenge on ${subject}.`);
-    }
+  constructor(studentAttrs) {
+    super(studentAttrs);
+    this.previousBackground = studentAttrs.previousBackground;
+    this.className = studentAttrs.className;
+    this.favSubjects = studentAttrs.favSubjects;
+  }
+  listsSubjects(subject1, subject2, subject3) {
+    console.log(
+      `Their favorite subjects are ${subject1}, ${subject2}, and ${subject3}.`
+    );
+  }
+  PRAssignment(subject) {
+    console.log(`${this.name} has submitted a PR for ${subject}.`);
+  }
+  sprintChallenge(subject) {
+    console.log(`${this.name} has begun sprint challenge on ${subject}.`);
+  }
 }
-
-
 
 /* 
 
+#### Project Manager
 * Now that we have instructors and students, we'd be nowhere without our PM's
 * ProjectManagers are extensions of Instructors
 * ProjectManagers have the following unique props:
@@ -114,15 +112,103 @@ class Student extends Person {
 
 // GrandChild Class -- Project Managers
 class ProjectManager extends Instructor {
-    constructor(pmAttrs) {
-        super(pmAttrs);
-        this.gradClassName = pmAttrs.gradClassName;
-        this.favInstructor = pmAttrs.favInstructor;
-    }
-    standUp(channel) {
-        console.log(`${this.name} announces to ${channel}, @channel standy times!`);
-    }
-    debugsCode(student, subject) { //maybe remove student?
-        console.log(`${this.name} debugs ${student.name}'s code on ${subject}.`);
-    }
+  constructor(pmAttrs) {
+    super(pmAttrs);
+    this.gradClassName = pmAttrs.gradClassName;
+    this.favInstructor = pmAttrs.favInstructor;
+  }
+  standUp(channel) {
+    console.log(`${this.name} announces to ${channel}, @channel standy times!`);
+  }
+  debugsCode(student, subject) {
+    //maybe remove student?
+    console.log(`${this.name} debugs ${student.name}'s code on ${subject}.`);
+  }
 }
+
+// OBJECTS
+// all: name, age, location, gender
+// all method: speak()
+// instructors: specialty, favLanguage, catchPhrase
+// instructor methods: demo('subject'), grade(student, 'subject')
+// students: previousBackground, className ie CS132, favSubjects []
+// student methods: listSubjects(s1-s3), PRAssignment(subject), sprintChallenge(subject)
+// PM: instructors + gradClassName ie CS1, favInstructor ie Fred
+// PM methods: standUP ('channel'), debugsCode(student, 'subject')
+const nelly = new Person({
+  name: "Nelly",
+  age: 54,
+  location: "Spanaway",
+  gender: "female"
+});
+
+const fred = new Instructor({
+  name: "Fred",
+  age: 37,
+  location: "Bedrock",
+  gender: "male",
+  specialty: "Front-end",
+  favLanguage: "JavaScript",
+  catchPhrase: `Don't forget the homies`
+});
+
+const amarachi = new Student({
+  name: "Amarachi",
+  age: 24,
+  location: "Oakland",
+  gender: "female",
+  previousBackground: "Public Health",
+  className: "web18",
+  favSubjects: ["HTML", "CSS", "Javascript"]
+});
+
+const chase = new ProjectManager({
+  name: "Chase",
+  age: 20,
+  location: "Texas",
+  gender: "male",
+  specialty: "Back-end",
+  favLanguage: "HTML",
+  catchPhrase: `You got this, guys.`,
+  gradClassName: "web15",
+  favInstructor: "Josh"
+});
+
+
+// TESTING
+//person
+console.log(nelly.name); // Nelly
+console.log(nelly.speak()); // Hello my name is Nelly, I am from Spanaway.
+
+
+
+//instructor
+console.log(fred.name); // Fred
+console.log(fred.speak()); // Hello my name is Fred, I am from Bedrock.
+console.log(fred.specialty); // Front-end
+console.log(fred.catchPhrase);
+console.log(fred.demo("javascript")); // Today we are learning about javascript!
+console.log(fred.grade(amarachi, "javascript fundamentals")); // Amarachi receives a perfect score on javascript!!!
+
+
+//PM
+console.log(chase.name); // Chase
+console.log(chase.speak()); // Hello my name is Chase, I am from Texas.
+console.log(chase.specialty); // Back-end
+console.log(chase.catchPhrase); // You got this, guys.
+console.log(chase.gradClassName); // web15
+console.log(chase.favInstructor); // Josh
+console.log(chase.demo("iOS design")); // Today we are learning about iOS design!
+console.log(chase.grade(amarachi, "responsive design")); // Amarachi receives a perfect score on responsive design!!!
+console.log(chase.standUp("web18_chase")); // Chase announces to web18_chase, @channel standy times!
+console.log(chase.debugsCode(amarachi, "Applied Javascript")); // Chase debugs Amarachi's code on Applied Javascript.
+
+
+//student
+console.log(amarachi.name); //Amarachi
+console.log(amarachi.speak()); //Hello my name is Amarachi, I am from Oakland.
+console.log(amarachi.previousBackground); // Public Health
+console.log(amarachi.className); // web18
+console.log(amarachi.favSubjects); // ["HTML", "CSS", "Javascript"]
+// console.log(amarachi.listsSubjects(favSubjects));
+// console.log(amarachi.listsSubjects.apply(favSubjects[0], favSubjects[1], favSubjects[2]));
